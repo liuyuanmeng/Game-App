e77759fe4699330a699b13d9c32c0a2a317b10bb
Yuanmeng Liu <liuyuanmeng66@gmail.com>
2023-04-26 17:10:34 +0100

    Change logo image

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,10 @@
 import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/daisy.png'
+import logo from '../assets/game.png'
 import ColorModeSwitch from './ColorModeSwitch'
 
 const NavBar = () => {
   return (
-    <HStack  justifyContent='space-between' padding="10px">
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" borderRadius="full" />
       <ColorModeSwitch />
     </HStack>
