c08e65935b04bb2bb655af779248c41ce78b3c7a
Yuanmeng Liu <liuyuanmeng66@gmail.com>
2023-04-28 13:43:13 +0100

    build platform selector

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from '../hooks/useGames'
 import { Image } from '@chakra-ui/react'
 import getCroppedImageUrl from '../services/image-url'
 import CriticScore from './CriticScore'
+import PlatformIconList from './PlatformIconList'
 
 interface Props {
   game: Game
@@ -14,9 +15,9 @@ const GameCard = ({ game }: Props) => {
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between" marginBottom={3}>
-          {/* <PlatformIconList
+          <PlatformIconList
             platforms={game.parent_platforms?.map((p) => p.platform)}
-          /> */}
+          />
           <CriticScore score={game.metacritic} />
         </HStack>
       </CardBody>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -4,10 +4,17 @@ import { CanceledError } from 'axios'
 import useData from './useData'
 import { Genre } from './useGenres'
 
+export interface Platform {
+  id: number
+  name: string
+  slug: string
+}
+
 export interface Game {
   id: number
   name: string
   background_image: string
+  parent_platforms: { platform: Platform }[]
   metacritic: number
 }
 
