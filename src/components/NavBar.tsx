4b8b375a80f2319266702b4393047103dcce18fc
Yuanmeng Liu <liuyuanmeng66@gmail.com>
2023-05-02 14:55:32 +0100

    implement seraching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,14 @@ import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/game.png'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
-
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void
+}
+const NavBar = ({onSearch}: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" borderRadius="full" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   )
